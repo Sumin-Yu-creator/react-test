e16495c62c59a6cac4f6bf458a4ead66050e52c7
YuSumin <suminei@naver.com>
2022-08-17 12:17:42 +0900

    완료

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import ChatPage from "./components/ChatPage/ChatPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+
+function App(props) {
+
+  return (
+    <Routes>
+      <Route path="/" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
+export default App;
